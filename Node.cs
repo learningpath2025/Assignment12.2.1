5a50d503b8d352b618677f80e8d9ad8bf36fdc33
learningpath2025 <learningpath2025@outlook.com>
2025-11-11 14:51:39 -0600

    initial commit

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12._2._1
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int data = 0)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Assignment12._2._1
+{
+    internal class Program
+    {
+        public Node RemoveElements(Node head, int data)
+        {
+            Node dummy = new Node(0);
+            dummy.Next = head;
+            Node current = dummy;
+            while (current.Next != null)
+            {
+                if (current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+            return dummy.Next;
+        }
+        public static Node GetLinkedList()
+        {
+            Node head = new Node();
+            head.Next = new Node(2);
+            head.Next.Next = new Node(6);
+            head.Next.Next.Next = new Node(3);
+            head.Next.Next.Next.Next = new Node(4);
+            head.Next.Next.Next.Next.Next = new Node(5);
+            head.Next.Next.Next.Next.Next.Next = new Node(6);
+            return head;
+        }
+
+        public static void PrintNode(Node head)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Node head = GetLinkedList();
+            int data = 6;
+            Console.WriteLine("Original LinkedList:");
+            PrintNode(head);
+            Console.WriteLine($"Value: {data}");
+            Node newHead = new Program().RemoveElements(head, data);
+            Console.WriteLine("New LinkedList:");
+            PrintNode(newHead);
+        }
+    }
+}
